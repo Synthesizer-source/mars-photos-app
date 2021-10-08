1a5f2d8b6d9cf174f7a0702110e63c821f0eea62
Synthesizer-source <56120358+Synthesizer-source@users.noreply.github.com>
2021-10-08 15:57:45 +0300

    add shimmer effect

--- app/src/main/java/com/synthesizer/source/mars/util/BindingAdapters.kt
+++ app/src/main/java/com/synthesizer/source/mars/util/BindingAdapters.kt
@@ -2,11 +2,8 @@ package com.synthesizer.source.mars.util
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
 
 @BindingAdapter("app:load")
 fun ImageView.loadImage(url: String?) {
-    url?.let {
-        Glide.with(context).load(it).into(this)
-    }
+    load(url)
 }
--- app/src/main/java/com/synthesizer/source/mars/util/ImageViewExt.kt
+++ app/src/main/java/com/synthesizer/source/mars/util/ImageViewExt.kt
@@ -2,7 +2,26 @@ package com.synthesizer.source.mars.util
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 
-fun ImageView.load(url: String) {
-    Glide.with(context).load(url).into(this)
+private val shimmer by lazy {
+    Shimmer.AlphaHighlightBuilder()
+        .setDuration(1600)
+        .setBaseAlpha(0.7f)
+        .setHighlightAlpha(0.6f)
+        .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+        .setAutoStart(true)
+        .build()
+}
+
+fun ImageView.load(url: String?) {
+    val shimmerDrawable = ShimmerDrawable().apply {
+        setShimmer(shimmer)
+    }
+
+    Glide.with(context)
+        .load(url)
+        .placeholder(shimmerDrawable)
+        .into(this)
 }
